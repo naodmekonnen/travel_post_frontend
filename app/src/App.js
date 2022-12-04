7a6daa326549d8f0bb3a5bcd10c7a4c4e4db819e
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-12-04 19:22:37 +0000

    created file structure installed dependencies

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="text-6xl font-bold underline text-orange-700">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
