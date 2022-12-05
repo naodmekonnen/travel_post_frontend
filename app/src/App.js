7f49f73abc9316b8eb0771ff756a6f092d7b9b3a
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-12-05 14:58:20 +0000

    front end user authentication works

--- app/src/App.js
+++ app/src/App.js
@@ -4,21 +4,34 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Navbar from './components/Navbar';
 import Home from './components/Home'
+import { Outlet } from "react-router-dom";
+import { GlobalProvider } from './context/GlobalState';
+
+
 
 function App() {
   return (
     <div className="App">
 
+<GlobalProvider>
+
     <BrowserRouter>
     <Navbar />
-    <Routes>
-      <Route path='/home' element={<Home/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/register' element={<Register/>}></Route>
-    </Routes>
+
+      <h1>{process.env.REACT_APP_MYENVVAR}</h1>
+      <Outlet />
+      <Routes>
+        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+      </Routes>
     </BrowserRouter>
+    </GlobalProvider>
+
     </div>
   );
 }
 
 export default App;
+
+      
--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -9,11 +9,12 @@ const Register = () => {
     let navigate = useNavigate()
 
     const [user, setUser] = useState({
-        'first_name':'',
-        'last_name':'',
+        'firstName':'',
+        'lastName':'',
         'email':'',
         'username':'',
-        'password':''
+        'password':'',
+        'passwordConf':''
     })
 
     const handleChange = (key, value) => {
@@ -28,15 +29,6 @@ const Register = () => {
         navigate('/home')
     }
 
-    // const submitUser = () => {
-    //     axios.post('https://8000-naodmekonne-travelpostb-m4zblizjy31.ws-us77.gitpod.io/api/user/create/', {
-    //         'first_name':first_name,
-    //         'last_name':last_name,
-    //         'email':email,
-    //         'username':username
-    //     })
-    // }
-
     return (
         <div>
 
@@ -45,51 +37,51 @@ const Register = () => {
 
                     <form onSubmit={handleRegister}className="p-12 md:p-24">
                         <div className="flex items-center text-lg mb-6 md:mb-8">
-                            {/* <label htmlFor='first_name'>First name:</label> */}
+                            <label htmlFor='first_name'>First name:</label>
                             <input  className="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full"
                                 type="text" 
                                 id="firstname" 
                                 placeholder="First Name"
                                 required
-                                onChange={(e) =>handleChange('first_name', e.target.value)} 
+                                onChange={(e) =>handleChange('firstName', e.target.value)} 
                             />
                         </div>
                         <div className="flex items-center text-lg mb-6 md:mb-8">
-                            {/* <label htmlFor='lastname'>Last Name:</label> */}
+                            <label htmlFor='lastname'>Last Name:</label>
                             <input className="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" 
                                 type="text" 
                                 id="lastname"  
                                 placeholder="Last Name"
                                 required
-                                onChange={(e) =>handleChange('last_name', e.target.value)} 
+                                onChange={(e) =>handleChange('lastName', e.target.value)} 
  
                                 
                             />
                         </div>
                         <div className="flex items-center text-lg mb-6 md:mb-8">
-                            {/* <label htmlFor='email'>Email:</label> */}
+                            <label htmlFor='email'>Email:</label>
                             <input className="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" 
                                 type="email" 
                                 id="email" 
                                 placeholder="email"
                                 required 
-                                onChange={(e) =>handleChange('email', e.target.value)} 
+                                onChange={(e) => handleChange('email', e.target.value)} 
 
                             />
                         </div>
                         <div className="flex items-center text-lg mb-6 md:mb-8">
-                            {/* <label htmlFor='username'>Username:</label> */}
+                            <label htmlFor='username'>Username:</label>
                             <input className="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full"
                                 type="text" 
                                 id="username"  
                                 placeholder="Username"
                                 required
-                                onChange={(e) =>handleChange('username', e.target.value)} 
+                                onChange={(e) => handleChange('username', e.target.value)} 
  
                             />
                         </div>
                         <div className="flex items-center text-lg mb-6 md:mb-8">
-                            {/* <label htmlFor='pass'>Password:</label> */}
+                            <label htmlFor='pass'>Password:</label>
                             <input  className="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" 
                                 type="password" 
                                 id="password" 
@@ -97,19 +89,32 @@ const Register = () => {
                                 minLength='8'
                                 required
                                 onChange={(e) =>handleChange('password', e.target.value)} 
- 
                             />
                         </div>
+                        <div className="flex items-center text-lg mb-6 md:mb-8">
+                            <label htmlFor='pass'>Password:</label>
+                            <input  className="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" 
+                                type="password" 
+                                id="password" 
+                                placeholder="Password Confirmation"
+                                minLength='8'
+                                required
+                                onChange={(e) => handleChange('passwordConf', e.target.value)}
+                                />
+                        </div>
+                        
+                        
                         <button className="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full"
                         type='submit'
                         value='Register'
                         disabled={(
                             user.password &&
-                            user.password.length >=8 &&
-                            user.first_name &&
-                            user.last_name &&
-                            user.email)
-                            ? false : true
+                            user.password.length >= 8 &&
+                            user.password === user.passwordConf &&
+                            user.firstName &&
+                            user.lastName &&
+                            user.email
+                          ) ? false : true
                             }
                         
                         >Sign Up</button>
